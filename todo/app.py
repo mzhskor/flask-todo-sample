76a4b0133dec71892820c1cc423e3c0c4203eead
mzhskor <mzhsnico@gmail.com>
2020-07-26 23:17:34 +0900

    ルーティングやrender_templateを試した

--- todo/app.py
+++ todo/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 
@@ -10,6 +10,19 @@ def index():
 def hello():
     return 'Hello, World!'
 
-@app.route('/hi')
-def hi():
-    return 'abcde'
+@app.route('/user/<username>')
+def user(username):
+    return render_template('index.html', username=username)
+
+@app.route('/post/<int:post_id>')
+def post(post_id: int):
+    return f'{post_id=}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath: str):
+    return f'{subpath=}'
+
+@app.route('/redirect/')
+def redirect_test():
+    # /redirect -> /redirect/ にリダイレクトする
+    return f'test'
